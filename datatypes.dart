62c314e662731bf83e4f39f2f8361280b5042fdd
kishor-gahatraj <90193890+kishor-gahatraj@users.noreply.github.com>
2021-11-29 19:46:29 +0545

    datatypes

---
+++ datatypes.dart
@@ -0,0 +1,16 @@
+void main() {
+  int x = 10;
+  double d = 20.52;
+  String name = 'kishor';
+  bool programmer = true;
+  String address = 'Kathmandu';
+  bool val = (name == address);
+
+  Map books = new Map();
+  books['first'] = 'maths';
+  books['second'] = 'nepali';
+  books['thrid'] = 'english';
+
+  print(d);
+  print("$books , $x , $d ,$name, $programmer,$val");
+}
