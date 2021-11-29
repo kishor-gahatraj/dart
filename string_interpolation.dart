4165b95bf9e52411266dcc79c35fe8b1c9053c05
kishor-gahatraj <90193890+kishor-gahatraj@users.noreply.github.com>
2021-11-29 20:15:56 +0545

    string_interpolation

---
+++ string_interpolation.dart
@@ -0,0 +1,20 @@
+void main() {
+  //various String to define in dart
+  String a = 'single';
+  String b = "double";
+  String c = 'your\'s happiness';
+  String d = "your's happiness";
+
+  String e = "Nepal is a beautiful country. "
+      " Captial city of nepal is Kathmandu.";
+
+  print("This person is $a");
+  print("$b , $c , $d");
+  print(e);
+
+  int f = 20;
+  int g = 30;
+
+  print("The sum of f and g is ${f + g}");
+  print("The product of $f and $g is ${f * g}");
+}
