ffbc65873642a56616df9633b3afac6f1766920d
kishor-gahatraj <90193890+kishor-gahatraj@users.noreply.github.com>
2021-11-30 06:21:54 +0545

    constant.dart

---
+++ constant.dart
@@ -0,0 +1,14 @@
+void main() {
+  //final
+  final a = 20;
+  final b = 30;
+  final name = "Aakash";
+
+  //Const
+  const PI = 3.14;
+  const double x = 4.56;
+
+  print(a + b);
+  print(name);
+  print(PI + x);
+}
